2b52deb2106cc7f9f9cd97c699a01eafb5c132db
Nevil Mehta <nevilmehta5@gmail.com>
2022-08-15 03:19:15 +0530

    prev next update

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
           <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2022/08_aug/12_fri/img_1660300109270_423.jpg?":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetails" className="btn btn-sm btn-primary">Go somewhere</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Go somewhere</a>
           </div>
           </div>
       </div>
